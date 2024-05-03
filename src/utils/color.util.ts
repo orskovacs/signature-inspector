6695911ec818dd415ed84e1f09d5f11350d4eb1b
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-05-03 02:50:59 +0200

    Fix color generation issue

--- src/utils/color.util.ts
+++ src/utils/color.util.ts
@@ -1,2 +1,15 @@
+function getRandomNumberInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 export const getRandomColorHex = () =>
-  Math.floor(Math.random() * 16777215).toString(16)
+  [
+    getRandomNumberInRange(0, 256),
+    getRandomNumberInRange(0, 256),
+    getRandomNumberInRange(0, 256),
+  ]
+    .map((v) => {
+      const hex = v.toString(16)
+      return hex.length === 2 ? hex : `0${hex}`
+    })
+    .join('')
