fcee4dd623e0c20cf2b29bd2b5e30725d001e180
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-05-08 13:02:28 +0200

    Implement visulal representation element

--- src/elements/visual-representation.element.ts
+++ src/elements/visual-representation.element.ts
@@ -5,23 +5,50 @@ import {
   signaturesContext,
   SignatureData,
 } from '../contexts/signatures.context'
+import { ref } from 'lit/directives/ref.js'
 
 @customElement('visual-representation-element')
 export class VisualRepresentationElement extends LitElement {
   @consume({ context: signaturesContext, subscribe: true })
   private signatures!: SignatureData[]
 
-  private get visibleSignatures(): SignatureData[] {
-    return this.signatures.filter((s) => s.visible)
-  }
-
   render() {
-    return html`<div>${this.visibleSignatures.length}</div>
-      ${this.visibleSignatures.map(
-        (s) =>
-          html`<div .style="background-color: #${s.colorHex}">
-            ${s.colorHex}
-          </div>`
-      )}`
+    return html`<canvas
+      width="500px"
+      height="200px"
+      ${setupCanvas(this.signatures.filter((s) => s.visible))}
+    ></canvas>`
   }
+}
+
+function setupCanvas(signatures: SignatureData[]) {
+  return ref((element) => {
+    if (!(element instanceof HTMLCanvasElement)) {
+      return
+    }
+
+    const context = element.getContext('2d')!
+    context.clearRect(0, 0, 500, 200)
+    signatures.forEach((s) => {
+      context.strokeStyle = `#${s.colorHex}`
+      context.lineCap = 'round'
+
+      context.beginPath()
+      context.moveTo(
+        s.signature.dataPoints[0].xCoord,
+        s.signature.dataPoints[0].yCoord
+      )
+
+      s.signature.dataPoints.slice(1).forEach((p) => {
+        const pointSize = 0.1 + p.pressure * 7
+        const [x, y] = [p.xCoord, p.yCoord]
+
+        context.lineWidth = pointSize
+        context.lineTo(x, y)
+        context.stroke()
+        context.beginPath()
+        context.moveTo(x, y)
+      })
+    })
+  })
 }
