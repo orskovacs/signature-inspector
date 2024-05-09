4a47c3ace0a9b2c55e193983c3f35373e8949061
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-05-10 01:00:10 +0200

    Scale features for rendering

--- src/elements/visual-representation.element.ts
+++ src/elements/visual-representation.element.ts
@@ -9,13 +9,16 @@ import { ref } from 'lit/directives/ref.js'
 
 @customElement('visual-representation-element')
 export class VisualRepresentationElement extends LitElement {
+  static readonly canvasWidth = 700
+  static readonly canvasHeight = 200
+
   @consume({ context: signaturesContext, subscribe: true })
   private signatures!: SignatureData[]
 
   render() {
     return html`<canvas
-      width="500px"
-      height="200px"
+      width="${VisualRepresentationElement.canvasWidth}px"
+      height="${VisualRepresentationElement.canvasHeight}px"
       ${setupCanvas(this.signatures.filter((s) => s.visible))}
     ></canvas>`
   }
@@ -28,20 +31,36 @@ function setupCanvas(signatures: SignatureData[]) {
     }
 
     const context = element.getContext('2d')!
-    context.clearRect(0, 0, 500, 200)
+    context.clearRect(
+      0,
+      0,
+      VisualRepresentationElement.canvasWidth,
+      VisualRepresentationElement.canvasHeight
+    )
     signatures.forEach((s) => {
       context.strokeStyle = `#${s.colorHex}`
       context.lineCap = 'round'
 
+      const xMax = Math.max(...s.signature.dataPoints.map((p) => p.xCoord))
+      const yMax = Math.max(...s.signature.dataPoints.map((p) => p.yCoord))
+
+      const xStart =
+        (s.signature.dataPoints[0].xCoord / xMax) *
+        VisualRepresentationElement.canvasWidth
+      const yStart =
+        (s.signature.dataPoints[0].xCoord / yMax) *
+        VisualRepresentationElement.canvasHeight
+
       context.beginPath()
-      context.moveTo(
-        s.signature.dataPoints[0].xCoord,
-        s.signature.dataPoints[0].yCoord
-      )
+      context.moveTo(xStart, yStart)
 
       s.signature.dataPoints.slice(1).forEach((p) => {
-        const pointSize = 0.1 + p.pressure * 7
-        const [x, y] = [p.xCoord, p.yCoord]
+        const pressure = p.pressure <= 1 ? p.pressure : p.pressure / 1000
+        const pointSize = 0.1 + pressure * 7
+        const [x, y] = [
+          (p.xCoord / xMax) * VisualRepresentationElement.canvasWidth,
+          (p.yCoord / yMax) * VisualRepresentationElement.canvasHeight,
+        ]
 
         context.lineWidth = pointSize
         context.lineTo(x, y)
