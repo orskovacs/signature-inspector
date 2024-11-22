08f642e76f3b5a39ddcd310be8ce5d0c5e0e9f4d
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2024-11-22 12:35:30 +0100

    Run dotnet code from web app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import './style.css'
 import { applyCustomTheme } from './utils/theme.util'
+import { dotnet } from './dotnet-assembly-exports.ts'
+import { DotnetAssemblyExports } from './dotnet-assembly-exports.js'
 
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   applyCustomTheme()
 })
 
-google.charts.load('current', { packages: ['corechart', 'line', 'table'] })
+await google.charts.load('current', { packages: ['corechart', 'line', 'table'] })
+
+const is_browser = typeof window != 'undefined'
+if (!is_browser) throw new Error(`Expected to be running in a browser`)
+
+const { getAssemblyExports, getConfig } = await dotnet.create()
+
+const config = getConfig()
+const exports: DotnetAssemblyExports = await getAssemblyExports(
+  config.mainAssemblyName!
+)
+const text = exports.SignatureVerifier.JSExportContainer.Greet('Teszt Elek')
+console.log(text)
+
+// await dotnet.run()
