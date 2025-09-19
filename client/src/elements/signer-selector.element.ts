6d1de461fb275469f6effb892b1c9d17182c11d3
Örs Kovács <46872866+orskovacs@users.noreply.github.com>
2025-09-19 08:29:23 +0200

    Don't display generated signer ID's

--- client/src/elements/signer-selector.element.ts
+++ client/src/elements/signer-selector.element.ts
@@ -87,7 +87,7 @@ export class SignerSelectorElement extends LitElement {
         ${this.signers.map(
           (s, i) => html`
             <md-select-option value="${i}"
-            >${s.name} [${s.id}]
+            >${s.name}<!-- [${s.id}] -->
             </md-select-option>
           `
         )}
